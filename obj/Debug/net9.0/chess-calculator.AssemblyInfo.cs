adc856c0fa3652ab8365bc3f5be7618e6c50f37d
rook512 <31941582+rook512@users.noreply.github.com>
2025-07-01 14:50:19 -0500

    "Add .gitignore"

--- obj/Debug/net9.0/chess-calculator.AssemblyInfo.cs
+++ obj/Debug/net9.0/chess-calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chess-calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a9f15b4bf6c632b924d7f74aa3715a4695f8d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("chess-calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chess-calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
